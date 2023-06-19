92c14cef95e10a2e8d7822459297ac98bd545d8d
felipx <felipemonterobruni@gmail.com>
2023-06-19 04:13:12 -0300

    Se agrega test que escribe de 0 a 9.

---
+++ driver_test.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void)
+{
+    int fd = open("/dev/7segs", O_RDWR);
+
+    for (char i = 0x30; i < 0x40; i++) {
+        write(fd, &i, 1);
+        sleep(1);
+    }
+}
